d9ada66430e8f28bcc1da318b591e537bc6c2b59
Erno <erno.kahkonen@aalto.fi>
2019-11-19 16:42:49 +0200

    fixed por

--- food.py
+++ food.py
@@ -1,8 +1,8 @@
-import requests
+import requests, re
 from requests.exceptions import HTTPError,Timeout
 from datetime import datetime
 
-def por():
+def por(date=datetime.now()):
     try:
         response=requests.get("http://por.fi/Menu-Pitajanmaki",timeout=10)
     except Timeout:
@@ -13,11 +13,12 @@ def por():
         print('Other error occurred: ' + err)
     else:
         sep=str(datetime.now().day)+"."+str(datetime.now().month)
-        dataString=sep
+        dataString=sep+'\n'
         txt=response.text
         if sep in txt:
             txt=txt.split('<div id="Printable" class="printable">')[1].split(sep)[1].split("5.60")[0]
             txt=txt.replace("<br />","\n").replace("&nbsp","").replace(";","").replace("&ouml","ö").replace("&auml","ä")+"5.60"
+            txt=remove_tags(txt)
             dataString=dataString+txt
         else:
             dataString=dataString+"Not available."
@@ -39,7 +40,12 @@ def por_v():
         if sep in txt:
             txt=txt.split('<div id="Printable" class="printable">')[1].split(sep)[1].split("0.85")[0]
             txt=txt.replace("<br />","\n").replace("&nbsp","").replace(";","").replace("&ouml","ö").replace("&auml","ä")+"0.85"
+            txt=remove_tags(txt)
             dataString=dataString+txt
         else:
             dataString=dataString+"Not available."
     return dataString
+
+def remove_tags(txt):
+    clean=re.compile('<.*?>')
+    return re.sub(clean,'',txt)
